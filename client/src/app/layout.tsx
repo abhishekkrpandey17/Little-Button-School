735d37dad991109d207dc8db1b6de034b3dd826d
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2025-05-09 19:09:11 +0530

    Update:Update in code

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppProvider } from "./context/AppContext";
-import { Loader } from "lucide-react";
+import Loader from "./components/Loader";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
