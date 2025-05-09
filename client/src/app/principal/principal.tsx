dafd5490c5bf84b07e9480eac920b3f3ee212311
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2025-05-09 16:40:36 +0530

    Update:Update in code

--- client/src/app/principal/principal.tsx
+++ client/src/app/principal/principal.tsx
@@ -42,7 +42,7 @@ const PrincipalMessage = () => {
   if (!data)
     return <p className="text-center text-red-500 mt-10">Message not found.</p>;
 
-  console.log(`${process.env.NEXT_PUBLIC_API_BASE_URL_TEST}${data.image}`);
+  console.log(`${process.env.NEXT_PUBLIC_API_URL_TEST}${data.image}`);
   return (
     <main className="min-h-screen bg-gradient-to-r from-[#d6ecff] via-[#e0f3fc] to-[#fcd6e0] py-20 px-6 md:px-12">
       {/* Header */}
