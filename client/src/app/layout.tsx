7f8cdeb0c82f4a365b5da95c090b877bb549a172
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2025-05-09 19:09:56 +0530

    Update:Update in code

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Loader />
         <AppProvider>{children}</AppProvider>
       </body>
     </html>
