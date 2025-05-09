4c0a10797996dee287cd905fc7aecab253a49a40
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2025-05-09 19:13:40 +0530

    Update:Update in code

--- client/src/app/photos/page.tsx
+++ client/src/app/photos/page.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import Navbar from "../mainpage/navbar";
 import Footer from "../mainpage/footer";
 import PhotosAll from "./PhotosAll";
+import Loader from "../components/Loader";
 
 const page = () => {
   return (
     <>
+      <Loader />
       <Navbar />
       <PhotosAll />
 
