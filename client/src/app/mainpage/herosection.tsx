0f3071ce6d0ef87dac554ef038ce1179f409f60b
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2025-05-13 17:18:04 +0530

    Update: update in code

--- client/src/app/mainpage/herosection.tsx
+++ client/src/app/mainpage/herosection.tsx
@@ -42,12 +42,7 @@ const HeroSection = () => {
       ))}
 
       {/* Foreground Content */}
-      <motion.div
-        initial={{ scale: 0.3, opacity: 0 }}
-        animate={isInView ? { scale: 1.05, opacity: 1 } : {}}
-        transition={{ duration: 0.8 }}
-        className="absolute inset-0 flex flex-col items-center justify-center text-center px-4"
-      >
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4">
         <div className="bg-white/20 backdrop-blur-[2px] px-6 py-16 md:px-20 md:py-16 rounded-3xl shadow-xl border border-white/20 max-w-xl md:max-w-4xl">
           <h1 className="text-3xl md:text-5xl font-extrabold text-pink-500 drop-shadow-md leading-snug">
             Welcome to
@@ -85,7 +80,7 @@ const HeroSection = () => {
             </motion.button>
           </Link>
         </motion.div>
-      </motion.div>
+      </div>
     </section>
   );
 };
